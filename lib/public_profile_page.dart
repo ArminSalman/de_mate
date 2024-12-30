5d118a592ba3c670c4731b778e5d76893b0b3dcd
ArminSalman <armin0salman@gmail.com>
2024-12-30 14:48:14 +0300

    I am gonna cry

--- lib/public_profile_page.dart
+++ lib/public_profile_page.dart
@@ -6,7 +6,7 @@ import 'package:de_mate/search_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'components/user.dart';
+import 'services/user.dart';
 
 class PublicProfilePage extends StatefulWidget {
   const PublicProfilePage({super.key, required this.userMail});
